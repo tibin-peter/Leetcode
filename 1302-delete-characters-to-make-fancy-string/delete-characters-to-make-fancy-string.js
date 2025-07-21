59ecf1620360de5df42fcd4caaf72de01376080e
Tibin-Peter <tibinpeter0@gmail.com>
2025-07-21 19:27:53 +0530

    Time: 85 ms (34.37%) | Memory: 66.2 MB (63.54%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyString = function(s) {
+    let result= "";
+    let count=1;
+      for(let i= 0;i<s.length;i++){
+          if(s[i]===s[i-1]){
+          count++
+         }else {
+             count=1;
+         }
+         if (count <3){
+             result+=s[i];
+         }
+      }
+ return result
+};
