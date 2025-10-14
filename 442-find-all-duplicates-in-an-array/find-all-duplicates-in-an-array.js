cd8ee0816538c448c3bf0d896fb138900d27c679
Tibin-Peter <tibinpeter0@gmail.com>
2025-10-14 19:00:50 +0530

    Time: 19 ms (53.14%) | Memory: 76.3 MB (18.36%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -3,7 +3,13 @@
  * @return {number[]}
  */
 var findDuplicates = function(nums) {
-    const empty=new Set()
-  return nums.filter(n=>empty.size===empty.add(n).size)
+//     const empty=new Set()
+//   return nums.filter(n=>empty.size===empty.add(n).size)
+const empty=new Set()
+  const duplicate =[]
+  for (const n of nums){
+      empty.has(n)?duplicate.push(n):empty.add(n)
+  }
+  return duplicate
     
 };
