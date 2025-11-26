27272a131ad0ea8922d190b387510a49cdbf37a3
Tibin-Peter <tibinpeter0@gmail.com>
2025-11-26 22:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.05%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.go
@@ -0,0 +1,13 @@
+func moveZeroes(nums []int)  {
+    j:=0
+    for i:=0;i<len(nums);i++{
+      if nums[i]!=0{
+        nums[j]=nums[i]
+        j++
+      } 
+    }
+    for j<len(nums){
+        nums[j]=0
+        j++
+    }
+}
