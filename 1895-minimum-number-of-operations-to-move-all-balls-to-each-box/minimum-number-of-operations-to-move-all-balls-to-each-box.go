cb1d6f4c55da07a8744b458b28dbde5fbb206050
Tibin-Peter <tibinpeter0@gmail.com>
2026-05-28 11:23:04 +0530

    Time: 84 ms (11.36%) | Memory: 7.3 MB (30.68%) - LeetSync

---
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.go
@@ -0,0 +1,44 @@
+// func minOperations(boxes string) []int {
+//     n:=len(boxes)
+//     operations:=make([]int,n)
+
+//     //for left to right
+//         leftBall:=0
+//         leftOpr:=0
+//         for i:=0;i<n;i++{
+//             operations[i]+=leftOpr
+//             if boxes[i]=='1'{
+//                 leftBall++
+//             }
+//             leftOpr+=leftBall
+//         }
+//     // for Right-to-Left
+//         rightBalls := 0
+//         rightOps := 0
+//            for i := n - 1; i >= 0; i-- {
+//             operations[i] += rightOps
+//              if boxes[i] == '1' {
+//               rightBalls++
+//            }
+//             rightOps += rightBalls
+//        }
+//     return operations
+// }
+func minOperations(boxes string) []int {
+    n:=len(boxes)
+    operations:=make([]int,0,n)
+    for i:=0;i<n;i++{
+        count:=0
+        for index,v:=range boxes{
+            if v=='1'{
+                distance:=index-i
+                if distance<0{
+                    distance= -distance
+                }
+                count+=distance
+            }
+        }
+        operations=append(operations,count)
+    }
+    return operations
+}
