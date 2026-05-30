a9b6fb3c0a8f7d338bfb6867cc3766cdcf4676b2
Tibin-Peter <tibinpeter0@gmail.com>
2026-05-30 20:02:20 +0530

    Time: 7 ms (84.47%) | Memory: 11.8 MB (71.84%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.go
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.go
@@ -1,20 +1,42 @@
 func pivotArray(nums []int, pivot int) []int {
-    leftArray:=[]int{}
-    rightArray:=[]int{}
-    equalArray:=[]int{}
-    for _,v:=range nums{
-        if v<pivot{
-            leftArray=append(leftArray,v)
+    n:=len(nums)
+    result:=make([]int,n)
+    left:=0
+    right:=n-1
+    for i:=0;i<n;i++{
+        if nums[i]<pivot{
+            result[left]=nums[i]
+            left++
         }
-        if v==pivot{
-            equalArray=append(equalArray,v)
-        }
-        if v>pivot{
-            rightArray=append(rightArray,v)
+        j:=n-1-i
+        if nums[j]>pivot{
+            result[right]=nums[j]
+            right--
         }
     }
-    outPut:=slices.Concat(leftArray,equalArray,rightArray)
-
-    return outPut
-
+    for left <= right { //for pushing the qual ones
+        result[left] = pivot
+        left++
+    }
+    return result
 }
+// func pivotArray(nums []int, pivot int) []int {
+//     leftArray:=[]int{}
+//     rightArray:=[]int{}
+//     equalArray:=[]int{}
+//     for _,v:=range nums{
+//         if v<pivot{
+//             leftArray=append(leftArray,v)
+//         }
+//         if v==pivot{
+//             equalArray=append(equalArray,v)
+//         }
+//         if v>pivot{
+//             rightArray=append(rightArray,v)
+//         }
+//     }
+//     outPut:=slices.Concat(leftArray,equalArray,rightArray)
+
+//     return outPut
+
+// }
