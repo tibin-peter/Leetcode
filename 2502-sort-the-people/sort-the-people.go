6c362beea7a620fbb9ad3bdb4461954f8d91d7b9
Tibin-Peter <tibinpeter0@gmail.com>
2026-05-30 10:51:35 +0530

    Time: 2 ms (67.11%) | Memory: 9.8 MB (7.89%) - LeetSync

--- 2502-sort-the-people/sort-the-people.go
+++ 2502-sort-the-people/sort-the-people.go
@@ -1,12 +1,29 @@
 func sortPeople(names []string, heights []int) []string {
-    n:=len(names)
-    for i:=0;i<n;i++{
-        for j:=0;j<n-i-1;j++{
-            if heights[j]<heights[j+1]{
-                heights[j],heights[j+1]=heights[j+1],heights[j]
-                names[j],names[j+1]=names[j+1],names[j]
-            }
-        }
+    arr := make([]string, 0, len(names))
+    freq := make(map[int]string)
+
+    for i, v := range names {
+        freq[heights[i]] = v
     }
-    return names
+
+    sort.Slice(heights,func(i,j int)bool{
+        return heights[i]>heights[j]
+    })
+
+    for _,v:= range heights{
+        arr=append(arr,freq[v])
+    }
+    return arr
 }
+// func sortPeople(names []string, heights []int) []string {
+//     n:=len(names)
+//     for i:=0;i<n;i++{
+//         for j:=0;j<n-i-1;j++{
+//             if heights[j]<heights[j+1]{
+//                 heights[j],heights[j+1]=heights[j+1],heights[j]
+//                 names[j],names[j+1]=names[j+1],names[j]
+//             }
+//         }
+//     }
+//     return names
+// }
