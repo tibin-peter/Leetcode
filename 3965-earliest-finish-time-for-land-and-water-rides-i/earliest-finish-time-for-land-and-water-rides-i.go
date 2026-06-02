b1a585721a8b96b653073217c160524c56e12d35
Tibin-Peter <tibinpeter0@gmail.com>
2026-06-02 22:10:05 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (93.75%) - LeetSync

---
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.go
@@ -0,0 +1,44 @@
+func earliestFinishTime(landStartTime []int, landDuration []int, waterStartTime []int, waterDuration []int) int {
+    landTime:=landStartTime[0]+landDuration[0]
+    waterTime:=waterStartTime[0]+waterDuration[0]
+
+    for i:=0;i<len(landStartTime);i++{
+        landDur:=landStartTime[i]+landDuration[i]
+        if landDur<landTime{
+            landTime=landDur
+        }
+    }
+
+    for i:=0;i<len(waterStartTime);i++{
+        waterDur:=waterStartTime[i]+waterDuration[i]
+        if waterDur<waterTime{
+            waterTime=waterDur
+        }
+    }
+    bestLandFirstTime:=100000
+    for i:=0;i<len(waterStartTime);i++{
+        waterStart:=landTime
+        if waterStartTime[i]>waterStart{
+            waterStart=waterStartTime[i]
+        }
+        currentFinish:=waterStart+waterDuration[i]
+        if currentFinish<bestLandFirstTime{
+            bestLandFirstTime=currentFinish
+        }
+    }
+    bestWaterFirstTime:=100000
+    for i:=0;i<len(landStartTime);i++{
+        landStart:=waterTime
+        if landStartTime[i]>landStart{
+            landStart=landStartTime[i]
+        }
+        currentFinish:=landStart+landDuration[i]
+        if currentFinish<bestWaterFirstTime{
+            bestWaterFirstTime=currentFinish
+        }
+    }
+     if bestLandFirstTime < bestWaterFirstTime {
+        return bestLandFirstTime
+    }
+    return bestWaterFirstTime
+}
