cd66def4470072b259ff4e201462673cc04cbbf2
Tibin-Peter <tibinpeter0@gmail.com>
2026-06-02 10:42:43 +0530

    Time: 0 ms (100.00%) | Memory: 4.1 MB (89.09%) - LeetSync

---
+++ 4275-traffic-signal-color/traffic-signal-color.go
@@ -0,0 +1,12 @@
+func trafficSignal(timer int) string {
+    if timer == 0{
+        return "Green"
+    }
+    if timer == 30{
+        return "Orange"
+    }
+    if timer>30&&timer<=90{
+        return "Red"
+    }
+    return "Invalid"
+}
